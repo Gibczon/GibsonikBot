e81f8502cb2d881c8ab095b8e460c8029b4c838c
Maciek Dmoch <szampanek123@gmail.com>
2021-11-04 14:23:01 +0000

    Adding keep_alive to make it run 24/7 Adding zenquotes api for .quote command removing .hello command

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import os
 import discord
+import requests
+import json
+from keep_alive import keep_alive
 
 client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return quote
 
 @client.event
 async def on_ready():
@@ -12,7 +21,9 @@ async def on_message(message):
   if message.author == client.user:
     return;
   
-  if message.content.startswith('.hello'):
-    await message.channel.send("Hello")
-
+  if message.content.startswith('.quote'):
+    quote = get_quote()
+    await message.channel.send(quote)
+    
+keep_alive()
 client.run(os.environ['password'])
